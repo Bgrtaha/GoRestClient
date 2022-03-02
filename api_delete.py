a00dc61c9ce7c2c3d92eeb6fddfa9c088baa3966
Bgrtaha <100716787+Bgrtaha@users.noreply.github.com>
2022-03-02 14:23:49 +0300

    Delete api_delete.py

--- api_delete.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url= "https://gorest.co.in/public/v2/users/2436"
-headers = {'Authorization': 'Bearer a102bd6582f80eb0b2a7e6cce8b7ecbe4b35f7ddf8fade9225f6e6d279a4ee20'}
-
-result = requests.delete(url,headers=headers)
-print(result.status_code)
